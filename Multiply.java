6a396c426dd2a5a6c33ba5d9015e023bd89365a7
Aryan Rohit Adsul <aryanadsul29@gmail.com>
2024-11-21 16:10:39 +0530

    multiply.java added

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a=2;
+        int b=4;
+         int c=a*b;
+         System.out.println("Muliplication is "+c);
+    }
+}
